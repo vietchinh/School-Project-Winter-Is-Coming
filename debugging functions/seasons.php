b1ef203db21599f4ae2d61f772203a9d63201979
Vietchinh Pham <vietchinhpham@hotmail.com>
2016-12-06 00:23:56 +0100

    Huurrr durr typooooo fixessss Signed-off-by: Vietchinh Pham <vietchinhpham@hotmail.com>

--- debugging functions/seasons.php
+++ debugging functions/seasons.php
@@ -19,13 +19,13 @@
 		$spring = $fakedate >= mktime(0, 0, 0, 3, 21, $fakeyear) && $fakedate < mktime(0, 0, 0, 6, 21, $fakeyear);
 		$summer = $fakedate >= mktime(0, 0, 0, 6, 21, $fakeyear) && $fakedate < mktime(0, 0, 0, 10, 21, $fakeyear);
 		$autumn = $fakedate >= mktime(0, 0, 0, 10, 21, $fakeyear) && $fakedate < mktime(0, 0, 0, 12, 21, $fakeyear) ;
-		echo "is current date is larger than 21-12-currentyear OR smaller than 21-3-currentyear?		";
+		echo "is current date larger than 21-12-currentyear OR smaller than 21-3-currentyear? ";
 		echo var_dump($winter) . "<br />";
-		echo "is current date is larger than 03-21-currentyear AND smaller than 06-21-currentyear?		";
+		echo "is current date larger than 03-21-currentyear AND smaller than 06-21-currentyear? ";
 		echo var_dump($spring). "<br />";
-		echo "is current date is larger than 21-12-currentyear AND smaller than 10-21-currentyear+1?	";
+		echo "is current date larger than 21-12-currentyear AND smaller than 10-21-currentyear? ";
 		echo var_dump($summer). "<br />";
-		echo "is current date is larger than 10-21-currentyear AND smaller than 12-21-currentyear+1?	";
+		echo "is current date larger than 10-21-currentyear AND smaller than 12-21-currentyear? ";
 		echo var_dump($autumn). "<br />";
 		echo "<br />" . $season; 
 	?>
