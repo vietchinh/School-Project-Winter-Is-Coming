2e7e1331764da46915c325cd23fa74563a9692fa
Vietchinh Pham <vietchinhpham@hotmail.com>
2016-12-05 17:56:06 +0100

    Changed: Current Time comparison to the seasons Signed-off-by: Vietchinh Pham <vietchinhpham@hotmail.com>

--- home.php
+++ home.php
@@ -6,36 +6,39 @@
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 		<script src="javascript/Javascript.js"></script>
 		<title> WINTER IS COMING </title>
-		
+		<?php session_start(); ?>
 	</head>
 	<body>
 	<?php
 	require_once("array/array.php");
 			print_r($seasons);
 			echo date("j m") . "<br />";
-	$currentdate = getdate();
-	$fakedate = date("d m", mktime(0, 0, 0, 12, 21 , 0));
-	$year = date("Y L", mktime(0, 0, 0, 0, 0, 2021));
-	echo $year . "<br/>";
-	if (date("L", mktime(0, 0, 0, 0, 0, 2001)) == 1) {
-		echo  . "<br />";
-	}
-	else {
-		echo "NO LEAP YEAR";
-	}
-	if ($fakedate == date("d m", mktime(0, 0, 0, 12, 21, 0)) ){
-		echo "WINTER!";
-	}
-	elseif ($fakedate == date("d m", mktime(0, 0, 0, 3, 21, 0)) ){
-		echo "LENTE!";
-	}	
-	elseif ($fakedate == date("d m", mktime(0, 0, 0, 6, 21, 0)) ){
-		echo "ZOMER!";
-	}
-	elseif ($fakedate == date("d m", mktime(0, 0, 0, 10, 21, 0)) ){
-		echo "HERFTS!";
-	}	
+	
+	/* Debugging Area 
+	$fakeyear = date("Y", mktime(0, 0, 0, 1,  1, 2017));
+	$fakedate = time();
+		if ($fakedate >= mktime(0, 0, 0, 12, 21, $fakeyear) && $fakedate < mktime(0, 0, 0, 3, 21, $fakeyear + 1) || $fakedate <= mktime(0, 0, 0, 3, 20, $fakeyear)){
+			$season = "winter";
+		}
+		elseif ($fakedate >= mktime(0, 0, 0, 3, 21, $fakeyear) && $fakedate < mktime(0, 0, 0, 6, 21, $fakeyear)){
+			$season = "lente";
+		}	
+		elseif ($fakedate >= mktime(0, 0, 0, 6, 21, $fakeyear) && $fakedate < mktime(0, 0, 0, 10, 21, $fakeyear)){
+			$season = "zomer";
+		}
+		elseif ($fakedate >= mktime(0, 0, 0, 10, 21, $fakeyear) && $fakedate < mktime(0, 0, 0, 12, 21, $fakeyear)){
+			$season = "herfts";
+		}
+
+		$winter = $fakedate >= mktime(0, 0, 0, 12, 21, $fakeyear) && $fakedate < mktime(0, 0, 0, 3, 21, $fakeyear + 1) || $fakedate <= mktime(0, 0, 0, 3, 20, $fakeyear) ;
+		$lente = $fakedate >= mktime(0, 0, 0, 3, 21, $fakeyear) && $fakedate < mktime(0, 0, 0, 6, 21, $fakeyear);
+		$zomer = $fakedate >= mktime(0, 0, 0, 6, 21, $fakeyear) && $fakedate < mktime(0, 0, 0, 10, 21, $fakeyear);
+		$herfts = $fakedate >= mktime(0, 0, 0, 10, 21, $fakeyear) && $fakedate < mktime(0, 0, 0, 12, 21, $fakeyear) ;
+		echo var_dump($winter). "<br />" . var_dump($lente). "<br />" . var_dump($zomer) . "<br />" . var_dump($herfts); 
+		
+		echo $season; */
 	?>
+
 	<div class="title">
 		<h1>Autumn passes and one remembers one's reverence<h1>
 	</div>
