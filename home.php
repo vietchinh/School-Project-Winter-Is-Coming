4ea976b97233eb60c904faab22532a9ff224dab3
vietchinh <vietchinhpham@hotmail.com>
2016-12-05 12:01:35 +0100

    Initial files
    Signed-off-by: vietchinh <vietchinhpham@hotmail.com>

---
+++ home.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+		<link href="css/style.php" rel="stylesheet" type="text/css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+		<script src="Javascript.js"></script>
+		<title> WINTER IS COMING </title>
+	</head>
+	<body>
+	</body>
+</html>
