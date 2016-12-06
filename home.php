16287ed267129c20297cdf97a66c2d26a8e25b04
vietchinh <vietchinhpham@hotmail.com>
2016-12-06 10:25:49 +0100

    Added meta charset in home.php Signed-off-by: vietchinh <vietchinhpham@hotmail.com>

--- home.php
+++ home.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
 	<head>
+		<meta charset="UTF-8">
 		<link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
 		<link href="css/style.php" rel="stylesheet" type="text/css">
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
